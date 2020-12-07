a8ece0fdd160518153299f09cb59c2ab954a799e
Stephan Badragan <sbadragan@xmatters.com>
2020-12-06 18:01:22 -0800

    adding hammerspoon bindings for other stuff

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -10,6 +10,7 @@ local superKey = {"cmd", "alt", "ctrl", "shift"}
 -- app switching
 -------------------------------------------------------------------
 
+-- TODO: only show window on current screen or move window to current screen
 local function switchToApp(appName)
   local app = hs.application.find(appName)
   if (app and app:isFrontmost()) then
@@ -27,8 +28,8 @@ end)
 
 hs.hotkey.bind(superKey, "d", function() switchToApp('Brave Browser') end)
 hs.hotkey.bind(superKey, "e", function() switchToApp('IntelliJ IDEA') end)
+hs.hotkey.bind(superKey, "f", function() switchToApp('Finder') end)
 hs.hotkey.bind(superKey, "i", function() switchToApp('Parallels Desktop') end)
-hs.hotkey.bind(superKey, "j", function() switchToApp('TickTick') end)
 hs.hotkey.bind(superKey, "k", function() switchToApp('Fork') end)
 hs.hotkey.bind(superKey, "l", function() switchToApp('zoom.us') end)
 hs.hotkey.bind(superKey, "m", function() switchToApp('Monosnap') end)
@@ -36,6 +37,7 @@ hs.hotkey.bind(superKey, "o", function() switchToApp('Microsoft Outlook') end)
 hs.hotkey.bind(superKey, "r", function() switchToApp('Google Chrome') end)
 hs.hotkey.bind(superKey, "s", function() switchToApp('Slack') end)
 hs.hotkey.bind(superKey, "u", function() switchToApp('Bear') end)
+hs.hotkey.bind(superKey, "w", function() switchToApp('TickTick') end)
 hs.hotkey.bind(superKey, "return", function() switchToApp('kitty') end)
 hs.hotkey.bind({"cmd"}, "return", function() switchToApp('kitty') end)
 
@@ -66,6 +68,57 @@ end)
 hs.hotkey.bind(superKey, "]", function()
   local win = hs.window.focusedWindow()
   win:moveToScreen(getNextScreen(win))
+end)
+
+
+-------------------------------------------------------------------
+-- Moving between app windows
+-------------------------------------------------------------------
+
+function table_shallow_copy(t)
+  local t2 = {}
+  for k,v in pairs(t) do
+    t2[k] = v
+  end
+  return t2
+end
+
+function window_sort(w1, w2)
+  return w1:id() < w2:id()
+end
+
+local function getNextAppWindow(currentWindow)
+  local app = currentWindow:application()
+  local allWindows = app:allWindows()
+
+  -- get a list of sorted window ids
+  local ids = {}
+  for k, win in pairs(allWindows) do
+    if (win:id() ~= 0) then
+      table.insert(ids, win:id())
+    end
+  end
+  table.sort(ids)
+
+  -- get next windows id
+  local currentWinIndex = -1
+  for index, id in ipairs(ids) do
+    if (id == currentWindow:id()) then
+      currentWinIndex = index
+      break
+    end
+  end
+  local nextWinIndex = (currentWinIndex % (#ids)) + 1
+  local nextWinId = ids[nextWinIndex]
+
+  local nextWin = hs.window.find(nextWinId)
+  return nextWin
+end
+
+hs.hotkey.bind(superKey, "j", function()
+  local currentWindow = hs.window.focusedWindow()
+  local nextWin = getNextAppWindow(currentWindow)
+  nextWin:focus()
 end)
 
 

281460b808b0d56aea8eeea22e54e1a53a51be4a
Stephan Badragan <sbadragan@xmatters.com>
2021-02-11 10:54:00 -0800

    more config changes

---
+++ .finicky.js
@@ -0,0 +1,19 @@
+module.exports = {
+    defaultBrowser: "Brave Browser",
+    options: {
+        // Hide the finicky icon from the top bar. Default: false
+        hideIcon: false,
+        // Check for update on startup. Default: true
+        checkForUpdate: true,
+        // Change the internal list of url shortener services. Default: undefined
+        //urlShorteners: (list) => [...list, "custom.urlshortener.com"],
+        // Log every request with basic information to console. Default: false
+        logRequests: true
+    },
+    handlers: [
+        {
+            match: [/docs\.google\.com/],
+            browser: "Google Chrome"
+        },
+    ]
+};
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -11,7 +11,7 @@ local superKey = {"cmd", "alt", "ctrl", "shift"}
 -------------------------------------------------------------------
 
 local function switchToApp(appName)
-  local win = hs.window.focusedWindow()
+  local win = hs.window.frontmostWindow()
   local currentScreen = win:screen()
   local app = hs.application.find(appName)
   if (app and app:isFrontmost()) then
@@ -20,57 +20,6 @@ local function switchToApp(appName)
     hs.application.open(appName)
   end
 end
-
--- show name of application corresponding to current window
-hs.hotkey.bind(superKey, "a", function()
-  local win = hs.window.focusedWindow()
-  hs.alert.show(win:application():name())
-end)
-
-hs.hotkey.bind(superKey, "d", function() switchToApp('Brave Browser') end)
-hs.hotkey.bind(superKey, "e", function() switchToApp('IntelliJ IDEA') end)
-hs.hotkey.bind(superKey, "f", function() switchToApp('Finder') end)
-hs.hotkey.bind(superKey, "i", function() switchToApp('Parallels Desktop') end)
-hs.hotkey.bind(superKey, "k", function() switchToApp('Fork') end)
-hs.hotkey.bind(superKey, "l", function() switchToApp('zoom.us') end)
-hs.hotkey.bind(superKey, "m", function() switchToApp('Monosnap') end)
-hs.hotkey.bind(superKey, "o", function() switchToApp('Microsoft Outlook') end)
-hs.hotkey.bind(superKey, "r", function() switchToApp('Google Chrome') end)
-hs.hotkey.bind(superKey, "s", function() switchToApp('Slack') end)
-hs.hotkey.bind(superKey, "u", function() switchToApp('Bear') end)
-hs.hotkey.bind(superKey, "w", function() switchToApp('TickTick') end)
-hs.hotkey.bind(superKey, "return", function() switchToApp('kitty') end)
-hs.hotkey.bind({"cmd"}, "return", function() switchToApp('kitty') end)
-
-
--------------------------------------------------------------------
--- Moving windows between screens
--------------------------------------------------------------------
-
-local function getNextScreen(win)
-  local screens = hs.screen.allScreens()
-  local currentScreen = win:screen()
-  local currentScreenIndex = -1
-  for index, screen in ipairs(screens) do
-    if (screen == currentScreen) then
-      currentScreenIndex = index
-      break
-    end
-  end
-  local nextScreenIndex = (currentScreenIndex % (#screens)) + 1
-  return screens[nextScreenIndex]
-end
-
-hs.hotkey.bind(superKey, "[", function()
-  local win = hs.window.focusedWindow()
-  win:moveToScreen(getNextScreen(win))
-end)
-
-hs.hotkey.bind(superKey, "]", function()
-  local win = hs.window.focusedWindow()
-  win:moveToScreen(getNextScreen(win))
-end)
-
 
 -------------------------------------------------------------------
 -- Moving between app windows
@@ -88,6 +37,7 @@ function window_sort(w1, w2)
   return w1:id() < w2:id()
 end
 
+-- TODO: this is really slow for some reason
 local function getNextAppWindow(currentWindow)
   local app = currentWindow:application()
   local allWindows = app:allWindows()
@@ -116,33 +66,92 @@ local function getNextAppWindow(currentWindow)
   return nextWin
 end
 
-hs.hotkey.bind(superKey, "j", function()
-  local currentWindow = hs.window.focusedWindow()
-  local nextWin = getNextAppWindow(currentWindow)
-  nextWin:focus()
+-------------------------------------------------------------------
+-- Moving windows between screens
+-------------------------------------------------------------------
+
+local function getNextScreen(win)
+  local screens = hs.screen.allScreens()
+  local currentScreen = win:screen()
+  local currentScreenIndex = -1
+  for index, screen in ipairs(screens) do
+    if (screen == currentScreen) then
+      currentScreenIndex = index
+      break
+    end
+  end
+  local nextScreenIndex = (currentScreenIndex % (#screens)) + 1
+  return screens[nextScreenIndex]
+end
+
+-------------------------------------------------------------------
+-- Key Bindings
+-------------------------------------------------------------------
+
+hs.hotkey.bind(superKey, "a", function()
+  -- show name of application corresponding to current window
+  local win = hs.window.frontmostWindow()
+  hs.alert.show(win:application():name())
 end)
 
+-- superKey + b - cycle through windows of current app
 
--------------------------------------------------------------------
--- Window position/resizing
--------------------------------------------------------------------
+hs.hotkey.bind(superKey, "d", function() switchToApp('Brave Browser') end)
+hs.hotkey.bind(superKey, "e", function() switchToApp('IntelliJ IDEA') end)
+hs.hotkey.bind(superKey, "f", function() switchToApp('Finder') end)
+hs.hotkey.bind(superKey, "i", function() switchToApp('Parallels Desktop') end)
+--hs.hotkey.bind(superKey, "j", function()
+--  local currentWindow = hs.window.frontmostWindow()
+--  local nextWin = getNextAppWindow(currentWindow)
+--  nextWin:focus()
+--end)
+hs.hotkey.bind(superKey, "k", function() switchToApp('Fork') end)
+hs.hotkey.bind(superKey, "l", function() switchToApp('zoom.us') end)
+hs.hotkey.bind(superKey, "m", function() switchToApp('Mail') end)
+hs.hotkey.bind(superKey, "o", function() switchToApp('Fantastical') end)
+hs.hotkey.bind(superKey, "r", function() switchToApp('Google Chrome') end)
+hs.hotkey.bind(superKey, "s", function() switchToApp('Slack') end)
+hs.hotkey.bind(superKey, "u", function() switchToApp('Bear') end)
+hs.hotkey.bind(superKey, "v", function() switchToApp('Monosnap') end)
+hs.hotkey.bind(superKey, "w", function() switchToApp('TickTick') end)
+-- superkey + z - launches next meeting from Meeter Pro
 
+hs.hotkey.bind(superKey, "return", function() switchToApp('kitty') end)
+hs.hotkey.bind({"cmd"}, "return", function() switchToApp('kitty') end)
+
+-- move window to other screen
+hs.hotkey.bind(superKey, "[", function()
+  local win = hs.window.frontmostWindow()
+  win:moveToScreen(getNextScreen(win))
+end)
+
+-- move window to other screen
+hs.hotkey.bind(superKey, "]", function()
+  local win = hs.window.frontmostWindow()
+  win:moveToScreen(getNextScreen(win))
+end)
+
+-- move window to left side of screen
 hs.hotkey.bind(superKey, "left", function()
-  local win = hs.window.focusedWindow()
+  local win = hs.window.frontmostWindow()
   win:moveToUnit('[0,0 50x100]', 0)
 end)
 
+-- move window to right side of screen
 hs.hotkey.bind(superKey, "right", function()
-  local win = hs.window.focusedWindow()
+  local win = hs.window.frontmostWindow()
   win:moveToUnit('[50,0 50x100]', 0)
 end)
 
+-- maximize window
 hs.hotkey.bind(superKey, "up", function()
-  local win = hs.window.focusedWindow()
+  local win = hs.window.frontmostWindow()
   win:moveToUnit('[0,0 100x100]', 0)
 end)
 
+-- centered and 80% size window
 hs.hotkey.bind(superKey, "down", function()
-  local win = hs.window.focusedWindow()
+  local win = hs.window.frontmostWindow()
   win:moveToUnit('[10,10 80x80]', 0)
 end)
+

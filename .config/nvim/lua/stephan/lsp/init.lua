327c04c259d25d5bd847ed70353699fda56b031e
Stephan Badragan <sbadragan@xmatters.com>
2021-04-01 20:48:28 -0700

    added comment

--- .config/nvim/lua/stephan/lsp/init.lua
+++ .config/nvim/lua/stephan/lsp/init.lua
@@ -7,28 +7,40 @@
 -- configure LSP servers
 -- Pre-requisite for those working is to have servers installed.
 -- Commands to install them:
--- yarn global add eslint_d prettier_d stylelint_d typescript typescript-language-server
+-- yarn global add eslint_d prettier_d stylelint_d lua-fmt diagnostic-languageserver typescript typescript-language-server
 
 -- global handler customization
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
- vim.lsp.diagnostic.on_publish_diagnostics, {
-   -- Enable underline, use default values
-   underline = false,
-   -- Disable a feature
-   -- update_in_insert = false,
- }
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    -- Enable underline, use default values
+    underline = false
+    -- Disable a feature
+    -- update_in_insert = false,
+  }
 )
 
-
-require('stephan/lsp/sumneko_lua')
-require('stephan/lsp/vimls')
-require('stephan/lsp/tsserver')
-require('stephan/lsp/diagnosticls')
-require('stephan/lsp/saga')
+require("stephan/lsp/sumneko_lua")
+require("stephan/lsp/vimls")
+require("stephan/lsp/tsserver")
+require("stephan/lsp/diagnosticls")
+require("stephan/lsp/saga")
 
 -- define signs to show in the sign column
-vim.fn.sign_define("LspDiagnosticsSignHint", { text="", texthl="LspDiagnosticsSignHint" })
-vim.fn.sign_define("LspDiagnosticsSignInformation", { text="", texthl="LspDiagnosticsSignHint" })
-vim.fn.sign_define("LspDiagnosticsSignWarning", { text="", texthl="LspDiagnosticsSignWarning" })
-vim.fn.sign_define("LspDiagnosticsSignError", { text="✘", texthl="LspDiagnosticsSignError" })
-
+vim.fn.sign_define(
+  "LspDiagnosticsSignHint",
+  {text = "", texthl = "LspDiagnosticsSignHint"}
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignInformation",
+  {text = "", texthl = "LspDiagnosticsSignHint"}
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignWarning",
+  {text = "", texthl = "LspDiagnosticsSignWarning"}
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignError",
+  {text = "✘", texthl = "LspDiagnosticsSignError"}
+)

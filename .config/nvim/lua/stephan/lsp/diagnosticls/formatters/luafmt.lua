360bba2384c8e50094add38c26d5bdf6941cf7a0
Stephan Badragan <sbadragan@xmatters.com>
2021-04-01 20:47:15 -0700

    added lua formatter

---
+++ .config/nvim/lua/stephan/lsp/diagnosticls/formatters/luafmt.lua
@@ -0,0 +1,5 @@
+return {
+  command = "luafmt",
+  args = {"--stdin", "--line-width", "80", "--indent-count", "2"},
+  isStdout = true
+}
--- .config/nvim/lua/stephan/lsp/diagnosticls/init.lua
+++ .config/nvim/lua/stephan/lsp/diagnosticls/init.lua
@@ -1,15 +1,22 @@
-local lspconfig = require('lspconfig')
-local util = require('lspconfig/util')
-local handlers = require('stephan/lsp/handlers')
-local attach = require('stephan/lsp/attach')
-local eslint_linter = require('stephan/lsp/diagnosticls/linters/eslint')
-local stylelint_linter = require('stephan/lsp/diagnosticls/linters/stylelint')
-local eslint_formatter = require('stephan/lsp/diagnosticls/formatters/eslint')
-local prettier_formatter = require('stephan/lsp/diagnosticls/formatters/prettier')
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
+local handlers = require("stephan/lsp/handlers")
+local attach = require("stephan/lsp/attach")
+local eslint_linter = require("stephan/lsp/diagnosticls/linters/eslint")
+local stylelint_linter = require("stephan/lsp/diagnosticls/linters/stylelint")
+local eslint_formatter = require("stephan/lsp/diagnosticls/formatters/eslint")
+local prettier_formatter =
+  require("stephan/lsp/diagnosticls/formatters/prettier")
+local luafmt_formatter = require("stephan/lsp/diagnosticls/formatters/luafmt")
 
 lspconfig.diagnosticls.setup {
   -- cmd = {"diagnostic-languageserver", "--stdio", "--log-level", "4"},
-  root_dir = util.root_pattern("package.json", "jsconfig.json", ".git"),
+  root_dir = util.root_pattern(
+    "package.json",
+    "jsconfig.json",
+    ".git",
+    "stephanbadragan" -- home dir, could contain config for lua, etc.
+  ),
   filetypes = {
     "javascript",
     "javascriptreact",
@@ -18,48 +25,50 @@ lspconfig.diagnosticls.setup {
     "scss",
     "css",
     "sass",
-  };
+    "lua"
+  },
   init_options = {
     filetypes = {
-      javascript = 'eslint',
-      javascriptreact = 'eslint',
-      typescript = 'eslint',
-      typescriptreact = 'eslint',
-      css = 'stylelint',
-      scss = 'stylelint',
-      sass = 'stylelint'
+      javascript = "eslint",
+      javascriptreact = "eslint",
+      typescript = "eslint",
+      typescriptreact = "eslint",
+      css = "stylelint",
+      scss = "stylelint",
+      sass = "stylelint"
     },
     formatFiletypes = {
-      javascript = {'prettier', 'eslint'},
-      javascriptreact = {'prettier', 'eslint'},
+      javascript = {"prettier", "eslint"},
+      javascriptreact = {"prettier", "eslint"},
+      lua = {"luafmt"}
     },
     linters = {
       eslint = eslint_linter,
-      stylelint = stylelint_linter,
+      stylelint = stylelint_linter
     },
     formatters = {
       prettier = prettier_formatter,
-      eslint = eslint_formatter
-    },
-  };
+      eslint = eslint_formatter,
+      luafmt = luafmt_formatter
+    }
+  },
   on_init = function(client)
     client.resolved_capabilities.document_formatting = true
     client.resolved_capabilities.code_action = true
     client.resolved_capabilities.execute_command = true
-    client.request = handlers.client_request(client);
+    client.request = handlers.client_request(client)
   end,
   on_attach = attach.global_on_attach,
   handlers = {
     ["textDocument/codeAction"] = {
-      type = 'local_lsp',
+      type = "local_lsp",
       handler = handlers.lintCodeAction
     },
     ["workspace/executeCommand"] = {
-      type = 'local_lsp',
+      type = "local_lsp",
       handler = handlers.workspaceExecuteCommand
     },
     ["textDocument/publishDiagnostics"] = handlers.diagnosticlsPublishDiagnostics
     -- ["textDocument/publishDiagnostics"] = ReloadFunc('stephan/lsp/handlers', 'diagnosticlsPublishDiagnostics')
   }
 }
-

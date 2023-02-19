9496eb014059fe560b73d046e4e9c8ec8e16f4ab
Stephan Badragan <stephan.badragan@everbridge.com>
2023-02-18 17:39:54 -0800

    moved telescope

---
+++ .config/nvim/lua/my/plugins/telescope/init.lua
@@ -0,0 +1,16 @@
+return {
+  { "nvim-telescope/telescope.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope-fzf-native.nvim",    build = "make" },
+      { "fhill2/telescope-ultisnips.nvim" },
+      { "nvim-telescope/telescope-fzf-writer.nvim" },
+      { "nvim-telescope/telescope-ui-select.nvim" },
+      { "nvim-telescope/telescope-file-browser.nvim" },
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
+      { "nvim-telescope/telescope-dap.nvim" },
+    },
+    config = function()
+      require('my.plugins.telescope.telescope')
+    end
+  }
+}

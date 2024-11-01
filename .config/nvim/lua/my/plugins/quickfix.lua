7d7c7c3d34a04d4e7b9c1fa043948f5c07b35e15
Stephan Badragan <stephan.badragan@everbridge.com>
2024-11-01 11:56:42 -0700

    fixing background for nvim bqf

--- .config/nvim/lua/my/plugins/quickfix.lua
+++ .config/nvim/lua/my/plugins/quickfix.lua
@@ -1,36 +1,39 @@
 return {
   {
-    "kevinhwang91/nvim-bqf",
+    'kevinhwang91/nvim-bqf',
     lazy = true,
-    event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+    event = { 'BufReadPost', 'BufNewFile', 'VeryLazy' },
     config = function()
-      require("bqf").setup {
+      require('bqf').setup({
         auto_enable = true,
         magic_window = false, -- this creates issues when it's on and you have multiple splits
         preview = {
           win_height = 12,
           win_vheight = 12,
           delay_syntax = 80,
-          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" }
+          border_chars = { '┃', '┃', '━', '━', '┏', '┓', '┗', '┛', '█' },
+          winblend = 0,
         },
         func_map = {
-          vsplit = "",
-          ptogglemode = "z,",
-          stoggleup = ""
+          vsplit = '',
+          ptogglemode = 'z,',
+          stoggleup = '',
         },
         filter = {
           fzf = {
-            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " }
-          }
-        }
-      }
+            extra_opts = { '--bind', 'ctrl-o:toggle-all', '--prompt', '> ' },
+          },
+        },
+      })
     end,
     dependencies = {
-      { 'junegunn/fzf', build = function()
-        vim.fn['fzf#install']()
-      end
+      {
+        'junegunn/fzf',
+        build = function()
+          vim.fn['fzf#install']()
+        end,
       },
-      "treesitter"
-    }
+      'treesitter',
+    },
   },
 }

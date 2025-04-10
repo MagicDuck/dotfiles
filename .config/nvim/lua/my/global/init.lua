690becbbca63ea6240b96cdbf80172398eb8337f
Stephan Badragan <stephan.badragan@everbridge.com>
2025-04-10 00:44:29 -0700

    fixing deprecated

--- .config/nvim/lua/my/global/init.lua
+++ .config/nvim/lua/my/global/init.lua
@@ -2,3 +2,19 @@ my = { state = {} }
 require('my/global/inspect')
 require('my/global/keyMappings')
 require('my/global/commands')
+
+-- disable some annoying deprecation warnings
+local deprecate = vim.deprecate
+local ignore_deprecated = {
+  ['Defining diagnostic signs with :sign-define or sign_define()'] = 'vim.diagnostic.config()',
+  ['client.request'] = 'client:request',
+  ['vim.highlight'] = 'vim.hl',
+  ['vim.validate'] = 'vim.validate(name, value, validator, optional_or_msg)',
+}
+---@diagnostic disable-next-line: duplicate-set-field
+vim.deprecate = function(name, alternative, version, plugin, backtrace)
+  if ignore_deprecated[name] == alternative then
+    return
+  end
+  deprecate(name, alternative, version, plugin, backtrace)
+end

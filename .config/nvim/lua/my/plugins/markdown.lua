a839b07c03aff29b50b655a3a365004ad87652b4
Stephan Badragan <stephan.badragan@everbridge.com>
2026-07-30 23:40:22 -0700

    more markdown improv

--- .config/nvim/lua/my/plugins/markdown.lua
+++ .config/nvim/lua/my/plugins/markdown.lua
@@ -40,7 +40,9 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      render_modes = { 'n', 'c', 't', 'i' },
+    },
   },
 
   {
